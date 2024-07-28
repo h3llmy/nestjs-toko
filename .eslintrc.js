1a84c09df30419791e2e8a70ea1fd0cfb467eb4f
H3llmy <helmytrisna86@gmail.com>
2024-07-28 21:11:51 +0800

    update: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,22 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        prefix: ['I'],
+      },
+      {
+        selector: 'class',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'enum',
+        format: ['PascalCase'],
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
