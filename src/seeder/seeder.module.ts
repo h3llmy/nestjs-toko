581e0cdfb34a7dce65f988e1923b1853923ecf3c
H3llmy <helmytrisna86@gmail.com>
2024-08-04 15:28:23 +0800

    fix: autoload entity migration

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -1,33 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeederService } from './seeder.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmModuleConfig } from '@app/common/config/typeOrmModule.config';
-import { User } from '../domains/users/entities/user.entity';
-import { Role } from '../domains/roles/entities/role.entity';
-import { Product } from '../domains/products/entities/product.entity';
-import { ProductCategory } from '../domains/product-category/entities/product-category.entity';
-import { Permissions } from '../domains/permissions/entities/permission.entity';
-import { Order } from '../domains/orders/entities/order.entity';
-import { OrderDetails } from '../domains/orders/entities/orderDetails.entity';
-import { Inventory } from '../domains/inventories/entities/inventory.entity';
-import { Discount } from '../domains/discounts/entities/discount.entity';
+import { typeOrmModuleSeederConfig } from '@app/common/config/typeOrmModule.config';
 
-// TODO: make it auto load entity
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync(typeOrmModuleConfig),
-    TypeOrmModule.forFeature([
-      User,
-      Role,
-      Product,
-      ProductCategory,
-      Permissions,
-      Order,
-      OrderDetails,
-      Inventory,
-      Discount,
-    ]),
-  ],
+  imports: [TypeOrmModule.forRootAsync(typeOrmModuleSeederConfig)],
   providers: [SeederService],
 })
 export class SeederModule {}
