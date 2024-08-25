135624af9b4c89dd780e75a404563828f413ba9d
H3llmy <helmytrisna86@gmail.com>
2024-08-25 19:45:50 +0800

    fix: docker compose pipeline

--- src/libs/encryption/src/encrypt.service.spec.ts
+++ src/libs/encryption/src/encrypt.service.spec.ts
@@ -1,5 +1,5 @@
 import { EncryptionService } from './encryption.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { TestBed } from '@automock/jest';
 
 describe('EncryptionService', () => {
